df8eae5139df2d9d7425b6048b81edb29287b189
George Moroz <agricolamz@gmail.com>
2025-01-01 15:00:19 +0300

    update

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -11,3 +11,4 @@ grep -o '<[^>]*>\|{[^}]*}' "$2" | sort -u > "$2".mchar
 cut -f1 -d: "$2" | hfst-strings2fst -m "$2".mchar -j -o "$2".hfst
 hfst-compose "$2".hfst "$1" | hfst-fst2strings -o "$2".res
 diff -U0 <(sort -u "$2") <(sort -u "$2".res)
+echo "all tests passed"
