18f612f31d72542a92bd273bc7b59a335b4bd6db
George Moroz <agricolamz@gmail.com>
2025-01-05 02:37:24 +0300

    update

--- task_tests/encrypt_answers.R
+++ task_tests/encrypt_answers.R
@@ -4,6 +4,6 @@ files <- list.files("task_tests", pattern = "_decrypted.txt", full.names = TRUE)
 
 walk(files, function(file){
   safer::encrypt_file(infile = file, 
-                      key = "2025_morphological_transducers", 
+                      key = "", 
                       outfile = str_replace(file, "decrypted.txt", "encrypted.bin"))
 })
