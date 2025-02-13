543166883f723550a4fab15704a84c8e8ad73023
George Moroz <agricolamz@gmail.com>
2025-02-13 15:30:00 +0300

    update

--- task_tests/encrypt_answers.R
+++ task_tests/encrypt_answers.R
@@ -4,10 +4,10 @@ password <- read_lines("task_tests/secret.txt")
 
 list.files("task_tests", pattern = "_decrypted.txt", full.names = TRUE) |> 
   walk(function(file){
--    output_file <- str_replace(file, "decrypted.txt", "encrypted.bin")
+    output_file <- str_replace(file, "decrypted.txt", "encrypted.bin")
     if(file.access(output_file)+1) {
-      file.remove(output_file)  
-    }
+      file.remove(output_file)
+      }
     safer::encrypt_file(infile = file, 
                         key = password, 
                         outfile = output_file)
